4944f165dd4025a6678f457af3015a15125ee314
ayush <ayushsharma12345qwerty@gmail.com>
2026-05-05 18:44:36 +0530

    click event and function call

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,20 @@
-import Header from "./Header";
-import Fruit from "./Fruit";
-import Login,{Profile,UserKey} from "./Usercomponet";
 function App(){
-
-  const userName= "ayush";
-  let x=7;
-  let y=12;
-  const Name= "ayush sharma";
-
-const userObj1={
-    name:"nilay",
-    age:25,
-    city:"delhi"
+  function callFun(){
+    alert("Hello World")
   }
-  const userArray=['sam','jhon','doe']
-
-
-
-
-
+  const fruit=()=>{
+    alert("i am fruit")
+  }
+  const fun=(name)=>{
+    alert(`Hello ${name}`)
+  }
   return(
     <div>
-      <Header />
-      <Fruit/>
-      
-      <h1>Hello, React!</h1>
-      <h2>{sum()} ,{UserKey} ,{color()}
-        <hr/>
-        Welcome to React {userName}
-        <hr />
-        the sum of {x} and {y} is {x+y} 
-        {Name.toUpperCase()}
-        <hr />
-        <button onClick={()=> alert(`hello, ${userName}`)}>Click here</button>
-      </h2>
-      <Profile/>
-      <hr/>
-      {userObj1.name} is {userObj1.age} year old and live in {userObj1.city}
-      <hr/>
-      <Hello />
+      <h1>event function call</h1>
+      <button onClick={callFun}>Click me</button>
+      <button onClick={fruit}>Click me</button>
+      <button onClick={()=>fun("John")}>Click me</button>
     </div>
   )
 }
-function Hello(){
-  return(
-    <h1>Hello, World!</h1>
-  )
-}
-function sum(){
-  return 7+4;
-}
-function color(){
-  return "red";
-}
-
-
 export default App;
