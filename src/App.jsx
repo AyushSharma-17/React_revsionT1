6c1fc2534ddfb2c75841c61ed819ae31371ee123
ayush <ayushsharma12345qwerty@gmail.com>
2026-05-05 17:36:25 +0530

    js in jsx with curly braces

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,38 @@ function App(){
   const userName= "ayush";
   let x=7;
   let y=12;
+  const Name= "ayush sharma";
+
+const userObj1={
+    name:"nilay",
+    age:25,
+    city:"delhi"
+  }
+  const userArray=['sam','jhon','doe']
+
+
+
+
+
   return(
     <div>
       <Header />
       <Fruit/>
       
       <h1>Hello, React!</h1>
-      <h2>{sum()} ,{UserKey}
+      <h2>{sum()} ,{UserKey} ,{color()}
         <hr/>
         Welcome to React {userName}
         <hr />
         the sum of {x} and {y} is {x+y} 
+        {Name.toUpperCase()}
         <hr />
         <button onClick={()=> alert(`hello, ${userName}`)}>Click here</button>
       </h2>
       <Profile/>
+      <hr/>
+      {userObj1.name} is {userObj1.age} year old and live in {userObj1.city}
+      <hr/>
       <Hello />
     </div>
   )
@@ -33,4 +50,9 @@ function Hello(){
 function sum(){
   return 7+4;
 }
+function color(){
+  return "red";
+}
+
+
 export default App;
