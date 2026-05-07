a5b6fe75c27cc93e19ee7e077b37b6803f34bf07
ayush <ayushsharma12345qwerty@gmail.com>
2026-05-07 21:40:50 +0530

    controlled componnets

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from "react";
 
 function App() {
-  const [val, setVal] = useState("Ayush");
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [email, setEmail] = useState('');
   return (
     <>
       <h1>enter the username in input field</h1>
       <h2>
-        <input type="text" onChange={(event) => setVal(event.target.value)} placeholder="enter username"></input>
+        <input type="text" onChange={(event) => setName(event.target.value)} placeholder="enter username"></input>
       </h2>
-      <h1>{val}</h1>
-      <button onClick={() => setVal("")}>clear</button>
+      <h2>
+        <input type="text" onChange={(event) => setAge(event.target.value)} placeholder="enter age"></input>
+      </h2>
+      <h2>
+        <input type="text" onChange={(event) => setEmail(event.target.value)} placeholder="enter email"></input>
+      </h2>
+      <h1>Name := {name}</h1>
+      <h1>age :={age}</h1>
+      <h1>email:= {email}</h1>
+      <button onClick={() => { setName(''); setAge(''); setEmail('') }}>clear</button>
     </>
   )
 }
