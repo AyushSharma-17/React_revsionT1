8a6bf4f55726d7ea62ff4529909018d32450d2f6
ayush <ayushsharma12345qwerty@gmail.com>
2026-05-07 17:33:02 +0530

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
-import { useState } from "react";
+import User from "./User";
 function App() {
-  const [count, setCount] = useState(0);
+  let userName = "Ayush";
+  let age = 23;
+  let email = "ayush123@gmail.conm";
   return (
     <div>
-      <h1>useState with condition</h1>
-      <h2>{count}</h2>
-      <button onClick={() => setCount(count + 1)}>click to increamnet</button>
-      {
-        count == 0 ? <h1>condition 0</h1>
-          : count == 1 ? <h1>condition 1</h1>
-            : count == 2 ? <h1>condition 2</h1>
-              : count == 3 ? <h1>condition 3</h1>
-                : count == 4 ? <h1>condition 4</h1>
-                  : count == 5 ? <h1>condition 5</h1>
-                    : count == 6 ? <h1>condition 6</h1>
-                      : count == 7 ? <h1>condition 7</h1>
-                        : count == 8 ? <h1>condition 8</h1>
-                          : count == 9 ? <h1>condition 9</h1>
-                            : count == 10 ? <h1>condition 10</h1>
-                              : <h1>condition more than 10</h1>
-      }
+      <User name={userName} age={age} email={email} />
     </div>
   )
-}
-export default App;
+} export default App;
---
+++ src/User.jsx
@@ -0,0 +1,9 @@
+function User(props) {
+    return (
+        <>
+            <h1>name is {props.name}</h1>
+            <h1>age is {props.age}</h1>
+            <h1>email is {props.email}</h1>
+        </>
+    )
+} export default User;
