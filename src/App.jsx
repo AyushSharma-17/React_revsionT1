4d84f20f0c8ccf86e7aec03c0e11ec7f63b06924
ayush <ayushsharma12345qwerty@gmail.com>
2026-05-07 17:17:15 +0530

    condition with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import { useState } from "react";
-import Counter from "./Counter";
-import Toggle from "./toggle";
 function App() {
-  const [Fruit, setFruit] = useState("Apple");
-
-  const handleFruit = () => {
-    setFruit("Bannana")
-  }
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <h2>State useState in react {Fruit}</h2>
-      <h3>
-        <button onClick={handleFruit}>Click to change fruit</button>
-
-      </h3>
-      <h2><Counter /></h2>
-      <Toggle />
+      <h1>useState with condition</h1>
+      <h2>{count}</h2>
+      <button onClick={() => setCount(count + 1)}>click to increamnet</button>
+      {
+        count == 0 ? <h1>condition 0</h1>
+          : count == 1 ? <h1>condition 1</h1>
+            : count == 2 ? <h1>condition 2</h1>
+              : count == 3 ? <h1>condition 3</h1>
+                : count == 4 ? <h1>condition 4</h1>
+                  : count == 5 ? <h1>condition 5</h1>
+                    : count == 6 ? <h1>condition 6</h1>
+                      : count == 7 ? <h1>condition 7</h1>
+                        : count == 8 ? <h1>condition 8</h1>
+                          : count == 9 ? <h1>condition 9</h1>
+                            : count == 10 ? <h1>condition 10</h1>
+                              : <h1>condition more than 10</h1>
+      }
     </div>
-
   )
 }
 export default App;
