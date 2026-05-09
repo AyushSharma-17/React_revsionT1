6dcd7ca72c633a9db00b53fb0e4aca191126c275
ayush <ayushsharma12345qwerty@gmail.com>
2026-05-09 12:27:29 +0530

    nested array using map loop

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,45 @@
-import UserRev from "./UserRev";
 function App() {
-  const userData = [{
-    name: "ayush",
-    age: 23,
-    email: "ayush@gmail.com"
-  },
-  {
-    name: "aman",
-    age: 24,
-    email: "aman@gmail.com"
-  },
-  {
-    name: "ayushi",
-    age: 22,
-    email: "ayushi@gmail.com"
-  },
-  {
-    name: "kam",
-    age: 25,
-    email: "kam@gmail.com"
-  }]
+  const collegeData = [
+    {
+      name: "IIT",
+      city: "delhi",
+      website: "iit@delhi.com",
+      student: [
+        { name: "ayush", age: 20 },
+        { name: "rahul", age: 21 }
+      ]
+    },
+    {
+      name: "MIT",
+      city: "mumbai",
+      website: "mit@mumbai.com",
+      student: [
+        { name: "jay", age: 23 },
+        { name: "karan", age: 22 }
+      ]
+    }
+  ];
+
   return (
     <div>
-      <h1>Reuse component </h1>
-      {
-        userData.map((user, index) => (
-          <div key={index}>
-            <UserRev data={user} />
-          </div>
-        ))
-      }
+      <h1>Component in React</h1>
+      {collegeData.map((college, index) => (
+        <div key={index}>
+          <h2>name: {college.name}</h2>
+          <h2>city: {college.city}</h2>
+          <h2>website: {college.website}</h2>
+
+          <h3>Student Data:</h3>
+          {college.student.map((students, sIndex) => (
+            <ul key={sIndex}>
+              <li>name: {students.name}</li>
+              <li>age: {students.age}</li>
+            </ul>
+          ))}
+        </div>
+      ))}
     </div>
-  )
+  );
 }
+
 export default App;
