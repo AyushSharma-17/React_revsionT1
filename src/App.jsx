9537d149af0d637398d1f3063efd4acd42e8f63a
ayush <ayushsharma12345qwerty@gmail.com>
2026-05-09 16:38:59 +0530

    dyanamic stylinig with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import { useState } from "react";
-import Ncounter from "./ncounter";
-
 function App() {
-  const [count, setCount] = useState(0);
+  const [cardStyle, setCardStyle] = useState({
+    height: "500px",
+    width: "500px",
+    backgroundColor: "blue"
+  })
+  // const handleClick = () => {
+  //   setCardStyle({
+  //     height: "500px",
+  //     width: "500px",
+  //     backgroundColor: "red"
+  //   })
+  const updateTheme = (bgColor, textColor) => {
+    setCardStyle({ ...cardStyle, backgroundColor: bgColor, color: textColor });
+  }
+
+
   return (
-    <div style={{ color: 'red', backgroundColor: 'white' }}>
-      <h1>hello</h1>
-      <button onClick={() => setCount(count + 1)}> click to increment {count}</button>
-      <Ncounter count={count} />
+    <div>
+      <div style={cardStyle}>
+        <h1>hello here</h1>
+      </div>
+      <button onClick={() => updateTheme('red', 'green')}>click</button>
     </div>
   )
 }
-
 export default App;
