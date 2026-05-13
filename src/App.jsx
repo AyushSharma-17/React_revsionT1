aba7e0b85bea02fc619bf1de9b1a7357b8233cfe
ayush <ayushsharma12345qwerty@gmail.com>
2026-05-13 14:18:00 +0530

    use Transaction hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { useFormStatus } from "react-dom";
-import CustomerForm from "./CustomerForm";
+import { useTransition } from "react";
+
 function App() {
-  const handleSubmit = async () => {
-    await new Promise(res => setTimeout(res, 2000));
+  const [pending, startTransition] = useTransition();
+  const handleButton = () => {
+    startTransition(async () => {
+      await new Promise(res => setTimeout(res, 5000));
+      console.log("update");
+    })
   }
   return (
     <div>
-      <h1>useFormStatus hook in reactjs</h1>
-      <form action={handleSubmit}>
-        <CustomerForm />
-      </form>
+      <h1>useTransition hook</h1>
+      {
+        pending ?
+          <img style={{ width: "100px" }} src="https://static.vecteezy.com/system/resources/thumbnails/046/814/197/small_2x/minimalist-black-and-white-icon-bar-with-border-for-loading-animated-motion-graphic-illustration-design-trailer-great-for-web-or-app-elements-video.jpg" />
+          : null
+      }
+      <button disabled={pending} onClick={handleButton}>click me to upadate</button>
     </div>
   )
 }
